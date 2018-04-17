5dc76af2533ea7ce45b741c9dadb0e9e6e95aabd
Utku Demir <me@utdemir.com>
2018-04-18 07:53:55 +1200

    wip

--- serverless-batch/src/Network/Serverless/Batch.hs
+++ serverless-batch/src/Network/Serverless/Batch.hs
@@ -12,8 +12,11 @@ module Network.Serverless.Batch where
 --------------------------------------------------------------------------------
 import Pipes
 import Data.Hashable
+import Data.List (sort)
 import Data.Typeable
+import Data.These
 import Data.Constraint
+import Control.Monad
 import Control.Distributed.Closure
 import qualified Pipes.Prelude as P
 --------------------------------------------------------------------------------
@@ -31,18 +34,19 @@ data Dataset a where
     -> Closure (Dict (Typeable b))
     -> Closure (Dict (Typeable t))
     -> Closure (Dict (Hashable t))
+    -> Closure (Dict (Ord t))
     -> Closure (a -> t)
     -> Closure (b -> t)
     -> Dataset a
     -> Dataset b
-    -> Dataset (a, b)
+    -> Dataset (These a b)
 
 instance Show (Dataset a) where
   show (DRead _ _) = "Read[" ++ show (typeRep (Proxy @a)) ++ "]"
   show (DBoundary a) = show a ++ "-> Boundary"
   show (DMap c a) = show a ++ " -> Map[" ++ show (typeRep c) ++ "]"
   show (DFilter _ a) = show a ++ " -> Filter"
-  show (DJoin _ _ _ _ _ _ a b) = "(" ++ show a ++ ", " ++ show b ++ ") -> Join"
+  show (DJoin _ _ _ _ _ _ _ a b) = "(" ++ show a ++ ", " ++ show b ++ ") -> Join"
 
 data Action where
   Write :: Closure Sink -> FilePath -> Dataset a -> Action
@@ -66,7 +70,7 @@ data SimpleDataset a where
   SDWide
     :: Closure (Dict (Typeable a))
     -> Closure (Dict (Typeable b))
-    -> Closure (Pipe a (Int, b) IO ())
+    -> Closure (Pipe (Maybe a) (Int, b) IO ())
     -> SimpleDataset a
     -> SimpleDataset b
   SDReadCorresponding
@@ -84,31 +88,36 @@ simplify (DMap f d)
   = SDNarrow (static P.map `cap` f) (simplify d)
 simplify (DFilter f d)
   = SDNarrow (static P.filter `cap` f) (simplify d)
-simplify (DJoin dta dtb dtk dhk (ka :: Closure (a -> k)) (kb :: Closure (b -> k)) dsa dsb) =
+simplify (DJoin dta dtb dtk dhk dok (ka :: Closure (a -> k)) (kb :: Closure (b -> k)) dsa dsb) =
   case (unclosure dta, unclosure dtb, unclosure dtk) of
     (Dict, Dict, Dict) ->
       SDReadCorresponding
       (SDWide
         dta
         dta
-        (static (partitioner @a @k) `cap` dhk `cap` ka)
+        (static (\dhk dok ka -> pSort @a dok >-> pPartitionBy @a @k dhk ka) `cap` dhk `cap` dok `cap` ka)
         (simplify dsa))
       (SDWide
         dtb
         dtb
-        (static (partitioner @b @k) `cap` dhk `cap` kb)
+        (static (\dhk dok kb -> pSort @b dok >-> pPartitionBy @b @k dhk kb) `cap` dhk `cap` dok `cap` kb)
         (simplify dsb))
-      (static (combine @a @b))
+      (static (pMerge @a @b) `cap` ka `cap` kb)
+
+pPartitionBy ::
+     forall a b. Dict (Hashable b) -> (a -> b) -> Pipe a (Int, a) IO ()
+pPartitionBy Dict f = P.map $ \a -> (partition (f a), a)
   where
     partition :: Hashable j => j -> Int
     partition t = hash t `mod` 200
 
-    partitioner :: forall g j.
-                   Dict (Hashable j)
-                -> (g -> j)
-                -> Pipe g (Int, g) IO ()
-    partitioner Dict f = P.map $ \g -> (partition (f g), g)
+pMerge :: forall a b t. (a -> t) -> (b -> t) -> Pipe (Either (Maybe a) (Maybe b)) (These a b) IO ()
+pMerge = undefined
 
-    combine :: forall g j. Pipe (Either (Maybe g) (Maybe j)) (g, j) IO ()
-    combine = undefined
-
+pSort :: forall a. Dict (Ord a) -> Pipe (Maybe a) a IO ()
+pSort Dict = go []
+  where
+    go xs =
+      await >>= \case
+        Nothing -> forM_ (sort xs) yield
+        Just x -> go (x : xs)
