0ec9fc5c6dd1fae8c392453faaef5e49d41929b6
Utku Demir <me@utdemir.com>
2019-03-09 23:57:15 +1300

    Safer GHArchive

--- distributed-dataset-opendatasets/src/Control/Distributed/Dataset/OpenDatasets/GHArchive.hs
+++ distributed-dataset-opendatasets/src/Control/Distributed/Dataset/OpenDatasets/GHArchive.hs
@@ -12,15 +12,22 @@ module Control.Distributed.Dataset.OpenDatasets.GHArchive
 --------------------------------------------------------------------------------
 import           Conduit                                                  (ConduitT,
                                                                            ResourceT,
+                                                                           handleC,
+                                                                           throwM,
                                                                            (.|))
+import           Control.Exception
 import qualified Data.Conduit.Combinators                                 as C
 import qualified Data.Conduit.JSON.NewlineDelimited                       as NDJ
 import           Data.Conduit.Zlib                                        (ungzip)
+import qualified Data.Text                                                as T
 import           Data.Time.Calendar                                       (Day, fromGregorian,
                                                                            showGregorian)
+import           Data.Typeable
 import           Network.HTTP.Simple                                      (getResponseBody,
+                                                                           getResponseStatusCode,
                                                                            httpSource,
                                                                            parseRequest)
+import           Text.Printf
 --------------------------------------------------------------------------------
 import           Control.Distributed.Dataset
 import           Control.Distributed.Dataset.OpenDatasets.GHArchive.Types
@@ -33,17 +40,29 @@ ghArchive (start, end) =
           (allUrls start end)
     ) & dCoalesce ((fromEnum end - fromEnum start + 1) * 6)
 
-allUrls :: Day -> Day -> [String]
+allUrls :: Day -> Day -> [T.Text]
 allUrls start end = do
   date <- showGregorian <$> [start..end]
-  time <- show <$> [(0::Int)..23]
-  return $ "http://data.gharchive.org/" ++ date ++ "-" ++ time ++ ".json.gz"
+  time <- printf "%02d" <$> [(0::Int)..23]
+  let str = T.pack date <> "-" <> T.pack time
+  return $ "http://data.gharchive.org/" <> str <> ".json.gz"
 
-processUrl :: String -> ConduitT () GHEvent (ResourceT IO) ()
-processUrl url = do
-  req <- parseRequest url
-  httpSource req getResponseBody
-    .| ungzip
-    .| NDJ.eitherParser @_ @GHEvent
-    .| C.mapM (either fail return)
+processUrl :: T.Text -> ConduitT () GHEvent (ResourceT IO) ()
+processUrl url = handleC wrapEx $ do
+  req <- parseRequest (T.unpack url)
+  httpSource req call
+ where
+  call req = do
+    case getResponseStatusCode req of
+      200 -> getResponseBody req
+               .| ungzip
+               .| NDJ.eitherParser @_ @GHEvent
+               .| C.mapM (either fail return)
+      404 -> return ()
+      r   -> fail $ "Unexpected status code: " ++ show r
 
+  wrapEx =
+    throwM . GHArchiveException url . id @SomeException
+
+data GHArchiveException e = GHArchiveException T.Text e deriving (Typeable, Show)
+instance (Typeable e, Show e) => Exception (GHArchiveException e)
