7ae1770476ebbf9ad37432c5797b753f9f649b69
Utku Demir <me@utdemir.com>
2019-05-17 18:37:12 +1200

    Update nixpkgs and stack resolver

--- pkgs.nix
+++ pkgs.nix
@@ -1,7 +1,7 @@
 let
 nixpkgs = builtins.fetchGit {
   url = "https://github.com/NixOS/nixpkgs";
-  rev = "1da6717d567314921f6c53dd8b979b1d1b4fe956";
+  rev = "f38fbe0d4949209d1dee9194d14718defcf9aece";
 };
 in
 import nixpkgs { config = { allowBroken = true; }; }
