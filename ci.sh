57f621ab6a07427f94f94a4586fdeb9e37929b0c
Utku Demir <me@utdemir.com>
2019-07-10 17:06:54 +1200

    Add ci.sh

---
+++ ci.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o errtrace
+
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+
+function err {
+    echo "Test failed!"
+    exit 1
+}
+trap err ERR
+
+function trace() {
+  echo "! $@" >&2; $@
+}
+
+if [ "$USER" = "utdemir" ]; then 
+  function nix_build {
+    tmp="$(mktemp -d)"
+    trap "rm -r '$tmp'" EXIT
+    trace nix build -o $tmp/result $@
+    echo $tmp/result | trace cachix push utdemir
+  }
+else 
+  function nix_build {
+    trace nix build --no-link $@ 
+  }
+fi
+
+nix_build
+nix_build --argstr compiler ghc844
+trace stack --silent --no-nix test 
+trace stack --silent --no-nix test --stack-yaml nightly.yaml
+
+echo "All good."
