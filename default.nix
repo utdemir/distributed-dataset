19e7af7b8feec811cc2a05917acbd96fb9ca31d2
Utku Demir <me@utdemir.com>
2019-11-25 11:23:44 +1300

    Compile with musl

--- default.nix
+++ default.nix
@@ -2,14 +2,22 @@ let
 sources = import ./nix/sources.nix;
 in
 
-{ pkgs ? import sources.nixpkgs { config.allowBroken = true; }
+{ pkgsOrig ? import sources.nixpkgs { config.allowBroken = true; }
 , compiler ? "ghc865"
 }:
 
 let
-gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
+pkgsMusl = pkgsOrig.pkgsMusl;
+haskell = pkgsMusl.haskell;
+
+gitignore = pkgsMusl.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
 
 overlays = se: su: {
+  # `cabal2nix` from `pkgsOrig` instead of `pkgsMusl`.
+  callCabal2nix = name: src:
+    let nix = pkgsOrig.haskellPackages.haskellSrc2nix { inherit name src; };
+    in  se.callPackage "${nix}/default.nix";
+
   "distributed-dataset" =
     se.callCabal2nix
       "distributed-dataset"
@@ -21,9 +29,9 @@ overlays = se: su: {
                  "distributed-dataset-aws"
                  (gitignore ./distributed-dataset-aws)
                  {};
-    in  pkgs.haskell.lib.overrideCabal orig (_: {
-          extraLibraries = with pkgs; [
-            glibc glibc.static zlib.static
+    in  haskell.lib.overrideCabal orig (_: {
+          extraLibraries = with pkgsMusl; [
+            zlib.static
             (libffi.override { stdenv = makeStaticLibraries stdenv; })
             (gmp.override { withStatic = true; })
           ];
@@ -40,16 +48,41 @@ overlays = se: su: {
                  "example-gh"
                  (gitignore ./examples/gh)
                  {};
-    in  pkgs.haskell.lib.overrideCabal orig (_: {
-          extraLibraries = with pkgs; [
-            glibc glibc.static zlib.static
+    in  haskell.lib.overrideCabal orig (_: {
+          extraLibraries = with pkgsMusl; [
+            musl zlib.static
             (libffi.override { stdenv = makeStaticLibraries stdenv; })
             (gmp.override { withStatic = true; })
           ];
     });
+
+  # Tests don't compile with musl:
+  #   hGetContents: invalid argument (invalid byte sequence)
+  #   commitBuffer: invalid argument (invalid character)
+  # Most (all?) of them are property-based tests, and musl
+  # does not handle some non-standard UTF-8 encodings; so
+  # they probably all share the same underlying issue.
+  "blaze-builder" = haskell.lib.dontCheck su.blaze-builder;
+  "bsb-http-chunked" = haskell.lib.dontCheck su.bsb-http-chunked;
+  "code-page" = haskell.lib.dontCheck su.code-page;
+  "conduit" = haskell.lib.dontCheck su.conduit;
+  "foundation" = haskell.lib.dontCheck su.foundation;
+  "hedgehog" = haskell.lib.dontCheck su.hedgehog;
+  "memory" = haskell.lib.dontCheck su.memory;
+  "retry" = haskell.lib.dontCheck su.retry;
+  "shelly" = haskell.lib.dontCheck su.shelly;
+  "tasty-hedgehog" = haskell.lib.dontCheck su.tasty-hedgehog;
+  "yaml" = haskell.lib.dontCheck su.yaml;
+
+  # Haddock does not work with musl:
+  #   haddock: internal error: <stdout>: \
+  #     commitBuffer: invalid argument (invalid character)
+  #     hGetContents: invalid argument (invalid byte sequence)
+  "basement" = haskell.lib.dontHaddock su.basement;
+  "path-io" = haskell.lib.dontHaddock su.path-io;
 };
 
-haskellPackages = pkgs.haskell.packages.${compiler}.override {
+haskellPackages = haskell.packages.${compiler}.override {
   overrides = overlays;
 };
 
@@ -62,7 +95,7 @@ in rec
 
   inherit haskellPackages;
 
-  docs = pkgs.runCommand "distributed-dataset-docs" {
+  docs = pkgsMusl.runCommand "distributed-dataset-docs" {
     buildInputs =
       [ (haskellPackages.ghcWithPackages (hp: with hp;
           [ distributed-dataset distributed-dataset-aws distributed-dataset-opendatasets ]))
@@ -86,11 +119,10 @@ in rec
       example-gh
     ];
     buildInputs = with haskellPackages; [
-      cabal-install
-      ghcid
-      ormolu
-    ] ++ [
-      pkgs.niv
+      pkgsOrig.haskellPackages.cabal-install
+      pkgsOrig.haskellPackages.ghcid
+      pkgsOrig.haskellPackages.ormolu
+      pkgsOrig.niv
     ];
     withHoogle = true;
   };
